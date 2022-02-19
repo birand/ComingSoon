129ac03a99c895ad78aca010fc3282c2a423010d
Birand Önen <birandbosley@gmail.com>
2022-02-20 00:56:06 +0300

    header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,19 +9,19 @@ type Props = {
 const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div className="relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover">
     <Head>
-      <title>{title} | Themeptation</title>
+      <title>{title} | NFT MARKETPLACE</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
         name="description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
+        content="LXNFT NEW GENERATION NFT MARKETPLACE"
       />
       <meta property="og:title" content={`${title} | Themeptation`} />
       <meta
         property="og:description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
+        content="LXNFT NEW GENERATION NFT MARKETPLACE"
       />
-      <meta property="og:url" content="https://slioth.themepttation.net/" />
+      <meta property="og:url" content="https://lxnft.store" />
       <meta property="og:type" content="website" />
     </Head>
     {children}
