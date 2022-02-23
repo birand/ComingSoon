3f787359f10a44e578e829d797f6e5b68ab00d4e
Birand Önen <birandbosley@gmail.com>
2022-02-23 22:53:52 +0300

    gtag

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,24 +15,24 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
         name="description"
-        content="LXNFT NEW GENERATION NFT MARKETPLACE"
+        content="CoolNFT NEW GENERATION NFT MARKETPLACE"
       />
       <meta property="og:title" content={`${title} | Themeptation`} />
       <meta
         property="og:description"
-        content="LXNFT NEW GENERATION NFT MARKETPLACE"
+        content="CoolNFT NEW GENERATION NFT MARKETPLACE"
       />
       <meta property="og:url" content="https://lxnft.store" />
       <meta property="og:type" content="website" />
     </Head>
-    <Script src="https://www.googletagmanager.com/gtag/js?id=G-5X3F9K9BQJ" strategy="afterInteractive" />
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-FL8PVHPK9C" strategy="afterInteractive" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-5X3F9K9BQJ');
+          gtag('config', 'G-FL8PVHPK9C');
         `}
       </Script>
     {children}
